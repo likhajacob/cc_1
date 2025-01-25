1f0052335eaa62480ad632f30e46b2b5d9acf4c8
Likha Jacob <likha.jacob03@gmail.com>
2025-01-25 18:29:37 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,19 @@ var isActive = true; // Employee active status
 console.log("Employee Name:", employeeName, "| Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "| Type:", typeof employeeID);
 console.log("Is Active:", isActive, "| Type:", typeof isActive);
+
+// Task 2 - Primative Data Types
+let productName = "wireless keyboard";
+
+// Declare a constant for the product price
+const productPrice = 58.99;
+
+// Declare a variable for product availability
+var isAvailable = true;
+
+// Log each variable's value and its type to the console
+console.log("Product Name:", productName, "- Type:", typeof productName);
+console.log("Product Price:", productPrice, "- Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "- Type:", typeof isAvailable);
+
+
