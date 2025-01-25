cff5533fa3175391673185eeb9f7b1b1263fe2a1
Likha Jacob <likha.jacob03@gmail.com>
2025-01-25 16:04:14 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task1 - Declaring variables
+let employeeName = "Richard Raynold"; // Employee's name 
+const employeeID = 1; // Employee's ID 
+var isActive = true; // Employee active status
+
+// Log the values and their types to the console
+console.log("Employee Name:", employeeName, "| Type:", typeof employeeName);
+console.log("Employee ID:", employeeID, "| Type:", typeof employeeID);
+console.log("Is Active:", isActive, "| Type:", typeof isActive);
